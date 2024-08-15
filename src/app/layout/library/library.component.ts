688401973998593727bd9b1aba17dece183a742e
Aurio Rajaa <riorajaa2018@gmail.com>
2024-08-15 12:00:30 +0700

    testing

---
+++ src/app/layout/library/library.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import {FaIconComponent, FontAwesomeModule} from "@fortawesome/angular-fontawesome";
+import {RouterLink} from "@angular/router";
+
+@Component({
+  selector: 'app-library',
+  standalone: true,
+  imports: [
+    FaIconComponent,
+    FontAwesomeModule,
+    RouterLink
+  ],
+  templateUrl: './library.component.html',
+  styleUrl: './library.component.scss'
+})
+export class LibraryComponent {
+
+}
